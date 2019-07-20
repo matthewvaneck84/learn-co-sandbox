73c036bef981765f37cec5009cd0397e4fe59994
Matthew Van Eck <thematt84@yahoo.com>
2019-07-20 12:23:14 +0000

    Automatically backed up by Learn

---
+++ methods.rb
@@ -0,0 +1,31 @@
+#puts "Hello World!"
+#puts "Hello World!" 
+#puts "Hello World!" 
+#puts "Hello World!" 
+#puts "Hello World!"
+
+
+#message = "Hello World!" 
+#puts message
+#puts message
+#puts message 
+#puts message
+#puts message 
+
+
+#def say_hello_world_five_times
+  #puts "Hello World!"
+  #puts "Hello World!"
+ # puts "Hello World!"
+  #puts "Hello World!"
+  #puts "Hello World!"
+#end
+#say_hello_world_five_times
+
+
+def greeting 
+  puts "Hello World!"
+end 
+
+greeting
+greeting
